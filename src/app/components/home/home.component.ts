b332f7a73997a7a2533ffff69c80ed84ba285e5d
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-02-28 13:12:39 +0200

    Get data, display it to users, handle error cases

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { CountryItemComponent } from "../country-item/country-item.component";
 import { CountryListInfo } from "../../interfaces/country-info";
 import { CountriesService } from "../../services/countries.service";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CountryItemComponent],
+  imports: [CountryItemComponent, ButtonComponent, CommonModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
@@ -23,10 +25,10 @@ export class HomeComponent implements OnInit {
     this.countriesService.getAllCountryListData()
       .subscribe({
         next: (data) => {
-          this.countryItemList = data;
+          this.countryItemList = data; // if there's data, populating the countryItemList array
         },
         error: (error) => {
-          this.error = error.message;
+          this.error = error.message; // if error, assigns the message to error variable that will be printed out to users
         }
       })
   }
